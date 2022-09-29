317019969375c3b7ab936cac0bc0de25eb42fd6b
gael <gsimon@webgeoservices.com>
2022-09-29 16:01:39 +0200

    feat: EventEmitter to manage communication between components.

---
+++ src/utils/event_emitter.ts
@@ -0,0 +1,69 @@
+namespace EventEmitter {
+    export type Listener = (...args: any[]) => void
+
+    export interface Events {
+        [event: string]: Listener[]
+    }
+
+    export interface Emitter {
+        on(event: string, listener: Listener): () => void
+
+        off(event: string, listener: Listener): void
+
+        offAll(): void
+
+        emit(event: string, ...args: any[]): void
+
+        once(event: string, listener: Listener): () => void
+    }
+}
+
+export class EventEmitter implements EventEmitter.Emitter {
+    private readonly events: EventEmitter.Events = {
+        '*': [],
+    }
+
+    public on(event: string, listener: EventEmitter.Listener): () => void {
+        if (typeof this.events[event] !== 'object') {
+            this.events[event] = []
+        }
+
+        this.events[event].push(listener)
+
+        return () => this.off(event, listener)
+    }
+
+    public off(event: string, listener: EventEmitter.Listener): void {
+        if (typeof this.events[event] !== 'object') {
+            return
+        }
+
+        this.events[event] = this.events[event].filter(
+            (eventListener) => eventListener !== listener
+        )
+    }
+
+    public offAll(): void {
+        Object.keys(this.events).forEach(
+            (event: string) => (this.events[event] = [])
+        )
+    }
+
+    public emit(event: string, ...args: any[]): void {
+        if (typeof this.events[event] === 'object') {
+            ;[...this.events[event]].forEach((listener) => listener.apply(this, args))
+        }
+
+        ;[...this.events['*']].forEach((listener) =>
+            listener.apply(this, [event, ...args])
+        )
+    }
+
+    public once(event: string, listener: EventEmitter.Listener): () => void {
+        const remove: () => void = this.on(event, (...args: any[]) => {
+            remove()
+            listener.apply(this, args)
+        })
+        return remove
+    }
+}
