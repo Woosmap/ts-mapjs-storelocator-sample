0ca1cce969e9076233105992c1f60cfe8fb74a81
gael <gsimon@webgeoservices.com>
2022-09-29 16:06:01 +0200

    chore: put objectToQueryString outside loadscript

---
+++ src/utils/utils.ts
@@ -0,0 +1,10 @@
+export type StringMap = Record<string, string | number | boolean>;
+
+export function objectToQueryString(params: StringMap): string {
+    let queryString = "";
+    Object.keys(params).forEach((key) => {
+        if (queryString.length !== 0) queryString += "&";
+        queryString += key + "=" + params[key].toString();
+    });
+    return queryString;
+}
