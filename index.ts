3305121a99570b35a2aef336a717e110dbadd71e
gael <gsimon@webgeoservices.com>
2022-11-29 11:08:17 +0100

    chore: added default mapPadding

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-import StoreLocator from './src/App';
+import StoreLocator from "./src/App";
+import {mapPaddings} from "./src/configuration/map.config";
 
 window.addEventListener("DOMContentLoaded", () => {
-    const $storelocator = document.querySelector<HTMLDivElement>('#StoreLocator') as HTMLElement;
-    if ($storelocator) new StoreLocator({$target: $storelocator, initialState: {}});
+    const $storelocator = document.querySelector<HTMLDivElement>(
+        "#StoreLocator"
+    ) as HTMLElement;
+    if ($storelocator)
+        new StoreLocator({$target: $storelocator, initialState: {padding: mapPaddings.full}});
 });
